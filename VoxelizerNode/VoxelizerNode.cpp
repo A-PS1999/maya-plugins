ccf8b2e2034ad1c4ac086e93fa44996371a19369
A-PS1999 <sarnoldparra@gmail.com>
2025-10-12 12:49:23 +0100

    Progress on setup code for VoxelizerNode

---
+++ VoxelizerNode/VoxelizerNode.cpp
@@ -0,0 +1,48 @@
+﻿#include "VoxelizerNode.h"
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MFnTypedAttribute.h>
+
+static const MTypeId TYPE_ID = MTypeId(0x000BEEF6);
+static const MString TYPE_NAME = "voxelizernode";
+
+MObject VoxelizerNode::inputMeshObj;
+MObject VoxelizerNode::outputMeshObj;
+MObject VoxelizerNode::voxelWidthObj;
+MObject VoxelizerNode::voxelDistanceObj;
+
+static const float DEFAULT_VOXEL_WIDTH = 0.9f;
+static const float DEFAULT_VOXEL_DISTANCE = 1.0f;
+
+VoxelizerNode::VoxelizerNode() : MPxNode() {}
+VoxelizerNode::~VoxelizerNode() {}
+
+void* VoxelizerNode::Creator() {
+	return new VoxelizerNode();
+}
+
+MStatus VoxelizerNode::Initialize() {
+	MFnNumericAttribute numericAttr;
+	MFnTypedAttribute typedAttr;
+
+	voxelWidthObj = numericAttr.create("voxelWidth", "voxW", MFnNumericData::kFloat, DEFAULT_VOXEL_WIDTH);
+	numericAttr.setWritable(true);
+	numericAttr.setReadable(false);
+	numericAttr.setStorable(true);
+	numericAttr.setHidden(false);
+	numericAttr.setMin(0.1f);
+
+	voxelDistanceObj = numericAttr.create("voxelDistance", "voxD", MFnNumericData::kFloat, DEFAULT_VOXEL_DISTANCE);
+	numericAttr.setWritable(true);
+	numericAttr.setReadable(false);
+	numericAttr.setStorable(true);
+	numericAttr.setHidden(false);
+	numericAttr.setMin(0.1f);
+}
+
+MTypeId VoxelizerNode::GetTypeId() {
+	return TYPE_ID;
+}
+
+MString VoxelizerNode::GetTypeName() {
+	return TYPE_NAME;
+}
---
+++ VoxelizerNode/VoxelizerNode.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <maya/MPxNode.h>
+
+class VoxelizerNode : public MPxNode {
+
+public:
+	VoxelizerNode();
+	virtual ~VoxelizerNode() override;
+
+	virtual MStatus compute(const MPlug& plug, MDataBlock& dataBlock) override;
+
+	static void* Creator();
+	static MStatus Initialize();
+
+	static MTypeId GetTypeId();
+	static MString GetTypeName();
+
+private:
+	static MObject voxelWidthObj;
+	static MObject voxelDistanceObj;
+	static MObject inputMeshObj;
+	static MObject outputMeshObj;
+};
